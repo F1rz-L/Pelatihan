94b77f95a0001028308d9705f7d2a63efdc4981c
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-11-14 20:43:17 +0700

    a

---
+++ AnimasiCanvas/javascript.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.body.style.backgroundColor="red";
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    const canvasWidth = canvas.offsetWidth;
+    const canvasHeight = canvas.offsetHeight;
+
+    ctx.fillStyle = "#008000";
+    ctx.fillRect = (0,0,canvasWidth,canvasHeight);
+})
