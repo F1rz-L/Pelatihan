11e9f5c4fa1bb541a44befd5dd553f1ed02b1e4f
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-11-15 11:36:27 +0700

    E

---
+++ laravel/vaksinasi-app/app/Models/Vaccination.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vaccination extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'dose',
+        'date',
+        'society_id',
+        'spot_id',
+        'vaccine_id',
+        'doctor_id',
+        'officer_id',
+    ];
+}
