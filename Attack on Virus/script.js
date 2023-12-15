d30a6c88179ea6afd7d2c29063b378b9dd2365e2
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-12-15 09:26:36 +0700

    Drops of rain gets louder without you beside me

--- Attack on Virus/script.js
+++ Attack on Virus/script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() { 
     let gameState = 'instruction'
+    let intervalCountdown = null
     let playerName = ''
     
     // Event Listeners
+    // keyup Document
+    document.addEventListener('keyup', function(e){
+        if(e.key === 'Escape' && gameState == 'playing'){
+            pauseGame()
+        }
+        if(e.key === 'Escape' && gameState == 'pause'){
+            resumeGame()
+        }
+    })
+
+
     document.querySelector('.form-control').addEventListener('keyup', function(e){
         const btnPlay = document.getElementById('btn-play')
         if (e.target.value.trim() == ""){
@@ -16,23 +28,58 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault()
         playerName = document.querySelector('.form-control').value
         setActiveScreen('countdown-screen', true)
+        startCountdown()
     })
 
     // functions
-    const clearActiveScreen = () =>{
+    const startCountdown = () => {
+        let countdown = 3
+        const cd = document.querySelector('.countdown-counter')
+        cd.innerHTML = countdown
+        intervalCountdown = setInterval(() => {
+            countdown--
+            cd.innerHTML = countdown
+            if(countdown == 0){
+                clearInterval(intervalCountdown)
+                startGame()
+            }
+        }, 1000)
+    }
+
+    const clearActiveScreen = () => {
         const activeScreen = document.querySelectorAll('.screen.active')
         activeScreen.forEach(screen => {
             screen.classList.remove('active')
         })
     }
 
-    const setActiveScreen = (screenID, clear = false) =>{
+    const setActiveScreen = (screenID, clear = false) => {
         if(clear){
             clearActiveScreen()
         }
 
         const screen = document.getElementById(screenID)
         screen.classList.add('active')
+    }
+
+    const startGame = () => {
+        gameState = 'playing'
+        setActiveScreen('main-screen', true)
+    }
+
+    const pauseGame = () => {
+        gameState = 'pause'
+        setActiveScreen('pause-screen')
+    }
+
+    const resumeGame = () => {
+        gameState = 'playing'
+        setActiveScreen('main-screen', true)
+    }
+
+    const endGame = () => {
+        gameState = 'end'
+        setActiveScreen('end-screen', true)
     }
 
 
